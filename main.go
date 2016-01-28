1f8962ff3d98bc5bfe19df0bdbcba0e9b34e85ea
Maxim Bublis <satori@dropbox.com>
2016-01-28 20:51:12 +0000

    Update CLI to use recent SDK.

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/user"
 	"path"
+
+	"golang.org/x/oauth2"
 
 	"github.com/codegangsta/cli"
 	"github.com/dropbox/dropbox-sdk-go"
@@ -18,16 +22,76 @@ const (
 
 var dbx dropbox.Api
 
-func initDbx(c *cli.Context) error {
+func readToken(filePath string) (*oauth2.Token, error) {
+	b, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	var tok oauth2.Token
+	if json.Unmarshal(b, &tok) != nil {
+		return nil, err
+	}
+
+	if !tok.Valid() {
+		return nil, fmt.Errorf("Token %v is no longer valid", tok)
+	}
+
+	return &tok, nil
+}
+
+func saveToken(filePath string, token *oauth2.Token) {
+	if _, err := os.Stat(filePath); err != nil {
+		if !os.IsNotExist(err) {
+			return
+		}
+		// create file
+		b, err := json.Marshal(token)
+		if err != nil {
+			return
+		}
+		if err = ioutil.WriteFile(filePath, b, 0644); err != nil {
+			return
+		}
+	}
+}
+
+func initDbx(c *cli.Context) (err error) {
 	u, err := user.Current()
 	if u == nil || err != nil {
-		return err
+		return
 	}
 	filePath := path.Join(u.HomeDir, ".config", "dbxcli", configFileName)
-	if dbx, err = dropbox.OauthClient(appKey, appSecret, filePath, false); err != nil {
-		return err
+
+	conf := &oauth2.Config{
+		ClientID:     appKey,
+		ClientSecret: appSecret,
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://www.dropbox.com/1/oauth2/authorize",
+			TokenURL: "https://api.dropbox.com/1/oauth2/token",
+		},
 	}
-	return nil
+	token, err := readToken(filePath)
+	if err != nil {
+		fmt.Printf("1. Go to %v\n", conf.AuthCodeURL("state"))
+		fmt.Printf("2. Click \"Allow\" (you might have to log in first).\n")
+		fmt.Printf("3. Copy the authorization code.\n")
+		fmt.Printf("Enter the authorization code here: ")
+
+		var code string
+		if _, err = fmt.Scan(&code); err != nil {
+			return
+		}
+		token, err = conf.Exchange(oauth2.NoContext, code)
+		if err != nil {
+			return
+		}
+		saveToken(filePath, token)
+	}
+
+	dbx = dropbox.Client(token.AccessToken, false)
+
+	return
 }
 
 func main() {
