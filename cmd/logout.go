b1a0a6a56d54062c1001ad9c028c50791b3de49d
Dylan Waits <dylan@waits.io>
2016-07-03 16:33:42 -0700

    Add `logout` command
    The `logout` command deletes the saved access tokens, logging the user out
    of all accounts (personal and team).
    
    Closes #16.

---
+++ cmd/logout.go
@@ -0,0 +1,49 @@
+// Copyright © 2016 Dropbox, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"os"
+	"path"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+)
+
+func logout(cmd *cobra.Command, args []string) error {
+	dir, err := homedir.Dir()
+	if err != nil {
+		return err
+	}
+	filePath := path.Join(dir, ".config", "dbxcli", configFileName)
+
+	err = os.Remove(filePath)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// logoutCmd represents the logout command
+var logoutCmd = &cobra.Command{
+	Use:   "logout [flags]",
+	Short: "Log out of the current session",
+	RunE:  logout,
+}
+
+func init() {
+	RootCmd.AddCommand(logoutCmd)
+}
