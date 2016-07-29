29c56f2f1c8a5f6e52be637486d0e9ad129cee0c
Diwaker Gupta <diwaker@dropbox.com>
2016-07-29 09:34:36 -0700

    Expand test script to include more commands.
    Note: `get` seems broken at the moment.

--- contrib/test.sh
+++ contrib/test.sh
@@ -11,23 +11,28 @@ ${dbxcli} version > /dev/null
 echo "Testing du"
 ${dbxcli} du > /dev/null
 
-echo "Testing ls"
-${dbxcli} ls > /dev/null
-
-echo "Testing ls -l"
-${dbxcli} ls -l > /dev/null
-
-echo "Testing search"
-${dbxcli} search dropbox > /dev/null
-
 echo "Testing mkdir"
 d=dbxcli-$(date +%s)
 ${dbxcli} mkdir ${d} > /dev/null
 
-echo "Testing rmdir"
-${dbxcli} rmdir ${d} > /dev/null
+echo "Testing put"
+${dbxcli} put ${dbxcli} ${d}/dbxcli > /dev/null
 
-#echo "Testing cp"
-#${dbxcli} cp ${dbxcli} ${d}/dbxcli > /dev/null
+#echo "Testing get"
+#${dbxcli} get ${d}/dbxcli /tmp/dbxcli > /dev/null
+# Make sure files are the same
+#cmp --silent ${dbxcli} /tmp/dbxcli
+
+echo "Testing ls -l"
+${dbxcli} ls -l ${d} > /dev/null
+
+echo "Testing search"
+${dbxcli} search dropbox /${d} > /dev/null
+
+echo "Testing cp"
+${dbxcli} cp ${d}/dbxcli ${d}/dbxcli-new
+
+echo "Testing rmdir"
+${dbxcli} rmdir ${d}
 
 echo "All tests passed!"
