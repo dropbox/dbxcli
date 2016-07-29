c05e92a764c4c3f3611781f597edfa5b29cafb98
Diwaker Gupta <diwaker@dropbox.com>
2016-07-29 07:28:53 -0700

    Add a few more commands to test script.

--- contrib/test.sh
+++ contrib/test.sh
@@ -5,6 +5,9 @@ set -e
 dbxcli=$(realpath $1)
 echo "Testing binary at ${dbxcli}"
 
+echo "Testing version"
+${dbxcli} version > /dev/null
+
 echo "Testing du"
 ${dbxcli} du > /dev/null
 
@@ -16,5 +19,15 @@ ${dbxcli} ls -l > /dev/null
 
 echo "Testing search"
 ${dbxcli} search dropbox > /dev/null
+
+echo "Testing mkdir"
+d=dbxcli-$(date +%s)
+${dbxcli} mkdir ${d} > /dev/null
+
+echo "Testing rmdir"
+${dbxcli} rmdir ${d} > /dev/null
+
+#echo "Testing cp"
+#${dbxcli} cp ${dbxcli} ${d}/dbxcli > /dev/null
 
 echo "All tests passed!"
