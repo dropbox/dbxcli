256bf5ee8e1074ef8f88b48b2927f9869e47091c
Mark Michaelis <mark@IntelliTect.com>
2021-01-01 20:45:32 -0800

    Working implementation with defect related to output formating

--- cmd/ls.go
+++ cmd/ls.go
@@ -25,6 +25,17 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const deletedItemFormatString = "<<%s>>"
+
+const colorReset = "\033[0m"
+const colorRed = "\033[31m"
+const colorGreen = "\033[32m"
+const colorYellow = "\033[33m"
+const colorBlue = "\033[34m"
+const colorPurple = "\033[35m"
+const colorCyan = "\033[36m"
+const colorWhite = "\033[37m"
+
 // Sends a get_metadata request for a given path and returns the response
 func getFileMetadata(c files.Client, path string) (files.IsMetadata, error) {
 	arg := files.NewGetMetadataArg(path)
@@ -39,11 +50,12 @@ func getFileMetadata(c files.Client, path string) (files.IsMetadata, error) {
 	return res, nil
 }
 
-func printFolderMetadata(w io.Writer, e *files.FolderMetadata, longFormat bool) {
+func formatFolderMetadata(w io.Writer, e *files.FolderMetadata, longFormat bool) (string) {
+	text := fmt.Sprintf("%s/\t", e.PathDisplay)
 	if longFormat {
-		fmt.Fprintf(w, "-\t-\t-\t")
+		text = fmt.Sprintf("-\t-\t-\t") + text
 	}
-	fmt.Fprintf(w, "%s\t", e.PathDisplay)
+	return text
 }
 
 func printFileMetadata(w io.Writer, e *files.FileMetadata, longFormat bool) {
@@ -60,7 +72,28 @@ func printDeletedMetadata(w io.Writer, e *files.DeletedMetadata, longFormat bool
 	fmt.Fprintf(w, "%s\t", e.PathDisplay)
 }
 
+func typeof(v interface{}) string {
+	return fmt.Sprintf("%T", v)
+}
+
 func ls(cmd *cobra.Command, args []string) (err error) {
+
+	w := new(tabwriter.Writer)
+	w.Init(os.Stdout, 4, 8, 1, ' ', 0)
+
+	logOut := new(tabwriter.Writer)
+	logOut.Init(os.Stderr, 4, 8, 1, ' ', 0)
+	debug := func(message string) {
+		fmt.Fprint(w, string(colorCyan)+"DEBUG: "+message+string(colorReset)+"\n")
+	}
+	error := func(message string) {
+		fmt.Fprint(w, string(colorRed)+"DEBUG: "+message+string(colorReset)+"\n")
+	}
+
+	print := func(message string) {
+		fmt.Fprint(w, message)
+	}
+
 	path := ""
 	if len(args) > 0 {
 		if path, err = validatePath(args[0]); err != nil {
@@ -76,6 +109,7 @@ func ls(cmd *cobra.Command, args []string) (err error) {
 	arg.IncludeDeleted = arg.IncludeDeleted || onlyDeleted
 
 	res, err := dbx.ListFolder(arg)
+
 	var entries []files.IsMetadata
 	if err != nil {
 		switch e := err.(type) {
@@ -113,15 +147,41 @@ func ls(cmd *cobra.Command, args []string) (err error) {
 		}
 	}
 
+	// debug(fmt.Sprintf("entries.Count = %v", len(entries)))
+
+	// var items = make(map[string]files.IsMetadata)
+	// // See ListFolder documentation at https://godoc.org/github.com/dropbox/dropbox-sdk-go-unofficial/dropbox/files#Client
+	// for _, entry := range entries {
+	// 	switch f := entry.(type) {
+	// 	case *files.FileMetadata:
+	// 		if !onlyDeleted {
+	// 			items[f.PathLower] = entry
+	// 		}
+	// 	case *files.FolderMetadata:
+	// 		if !onlyDeleted {
+	// 			items[f.PathLower] = entry
+	// 		}
+	// 	case *files.DeletedMetadata:
+	// 		if !onlyDeleted {
+	// 			f.PathDisplay = fmt.Sprintf(deletedItemFormatString, f.PathDisplay)
+	// 			items[f.PathLower] = entry
+	// 		}
+	// 	default:
+	// 		error("Converting Array to Map on item was type other than FileMetadata, FolderMetadata, or DeletedMetadata")
+	// 		// Ignore
+	// 	}
+	// }
+
+	// debug(fmt.Sprintf("items.Count = %v", len(items)))
+
 	long, _ := cmd.Flags().GetBool("long")
-	w := new(tabwriter.Writer)
-	w.Init(os.Stdout, 4, 8, 1, ' ', 0)
 
 	itemCounter := 0
 	newLineCounter := 0
 	if long {
 		fmt.Fprint(w, "Revision\tSize\tLast modified\tPath\n")
 	}
+
 	for _, entry := range entries {
 		switch f := entry.(type) {
 		case *files.FileMetadata:
@@ -138,19 +198,62 @@ func ls(cmd *cobra.Command, args []string) (err error) {
 			revisionArg := files.NewListRevisionsArg(f.PathLower)
 			res, err := dbx.ListRevisions(revisionArg)
 			if err != nil {
-				// This is expected to be the folder of the item.
-				break
+				listRevisionError, ok := err.(files.ListRevisionsAPIError)
+				if ok {
+					// We have a ListRevisionsAPIERror
+					if listRevisionError.EndpointError.Path.Tag == files.LookupErrorNotFile {
+						// Don't treat a "not_file" error as fatal; recover by sending a
+						// get_metadata request for the same path and using that response instead.
+						var metaRes files.IsMetadata
+						metaRes, err = getFileMetadata(dbx, f.PathLower)
+						if err != nil {
+							error("getFileMetadata failed with error:" + string(err.Error()))
+							break
+						}
+						debug("metaRes.(Type) = " + typeof(metaRes))
+						switch x := entry.(type) {
+						case *files.FileMetadata:
+							if !onlyDeleted {
+								debug("Retrieved revision for deleted File: " + x.PathLower)
+								printFileMetadata(w, x, long)
+								itemCounter = itemCounter + 1
+							}
+						case *files.FolderMetadata:
+							if !onlyDeleted {
+								debug("Retrieved revision for deleted Folder: " + x.PathLower)
+								printFolderMetadata(w, x, long)
+								itemCounter = itemCounter + 1
+							}
+						case *files.DeletedMetadata:
+							debug("Retrieved revision for deleted DeletedMetadata: " + x.PathLower)
+							f.PathDisplay = fmt.Sprintf(deletedItemFormatString, x.PathDisplay)
+							printDeletedMetadata(w, x, long)
+							itemCounter = itemCounter + 1
+						default:
+							error("Unexpected Type for mesRest = " + typeof(metaRes))
+						}
+					} else {
+						debug("listRevisionError.EndpointError.Path.Tag = " + listRevisionError.EndpointError.Path.Tag)
+					}
+				} else {
+					error("Unexpected Error type calling ListRevisions: " + err.Error())
+					break
+				}
 			} else if len(res.Entries) == 0 {
 				// Occasionally revisions will be returned with an empty Entries list.
-				f.PathDisplay = "*<" + f.PathDisplay + ">*"
+				f.PathDisplay = fmt.Sprintf(deletedItemFormatString, f.PathDisplay)
 				printDeletedMetadata(w, f, long)
+				itemCounter = itemCounter + 1
 			} else {
-				res.Entries[0].PathDisplay = "*<" + res.Entries[0].PathDisplay + ">*"
+				res.Entries[0].PathDisplay = fmt.Sprintf(deletedItemFormatString, res.Entries[0].PathDisplay)
 				printFileMetadata(w, res.Entries[0], long)
+				itemCounter = itemCounter + 1
 			}
-			itemCounter = itemCounter + 1
+		default:
+			debug("Item of unknown type (not FileMetadata, FolderMetadata, or DeletedMetadata) when iterating over all items")
+			// Ignore
 		}
-
+		// debug(fmt.Sprintf("itemCounter=%v; newLineCounter=%v", itemCounter, newLineCounter))
 		if (itemCounter%4 == 0) || (long && (itemCounter > newLineCounter)) {
 			fmt.Fprintln(w)
 			newLineCounter = newLineCounter + 1
@@ -158,6 +261,13 @@ func ls(cmd *cobra.Command, args []string) (err error) {
 	}
 
 	err = w.Flush()
+	if err != nil {
+		error("w.Flush():" + string(err.Error()))
+	}
+	err = logOut.Flush()
+	if err != nil {
+		error("logOut.Flush():" + string(err.Error()))
+	}
 	return err
 }
 
